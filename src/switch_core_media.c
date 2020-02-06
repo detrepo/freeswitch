57fd22aa9b4b2d467af533f37ae95c9dd0d89b73
21stcaveman <zetta22000@yahoo.com>
2020-02-06 10:18:24 -0800

    [core] Add in_jitter_variance and in_jitter_std_deviation to media statistics

--- src/switch_core_media.c
+++ src/switch_core_media.c
@@ -1985,6 +1985,8 @@ static void set_stats(switch_core_session_t *session, switch_media_type_t type, 
 		add_stat(stats->inbound.cng_packet_count, "in_cng_packet_count");
 		add_stat(stats->inbound.flush_packet_count, "in_flush_packet_count");
 		add_stat(stats->inbound.largest_jb_size, "in_largest_jb_size");
+		add_stat_double(stats->inbound.variance, "in_jitter_variance");
+		add_stat_double(stats->inbound.std_deviation, "in_jitter_std_deviation");
 		add_stat_double(stats->inbound.min_variance, "in_jitter_min_variance");
 		add_stat_double(stats->inbound.max_variance, "in_jitter_max_variance");
 		add_stat_double(stats->inbound.lossrate, "in_jitter_loss_rate");
